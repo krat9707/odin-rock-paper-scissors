66f881f092789b182113f9f704dac235eb5cfa93
krat <kashyapbaraiya9707@gmail.com>
2024-04-16 14:27:14 +0530

    Solve JS file linking problem

--- game.js
+++ game.js
@@ -1,115 +1,115 @@
-    function getComputerChoice()
-    {
-        let val = (Math.random() * 100);
+function getComputerChoice()
+{
+    let val = (Math.random() * 100);
 
-        if(val <= 33)
-            return "Rock";
-        else if(val <= 66)
-            return "Paper";
+    if(val <= 33)
+        return "Rock";
+    else if(val <= 66)
+        return "Paper";
+    else    
+        return "Scissors";
+}
+
+function roundResult(playerChoice, computerChoice)
+{
+    if(playerChoice == "rock")
+    {
+        if(computerChoice == "scissors")
+            return true;
+        else
+            return false;
+    }
+    else if(playerChoice == "paper")
+    {
+        if(computerChoice == "rock")
+            return true;
         else    
-            return "Scissors";
+            return false;
+    }
+    else 
+    {
+        if(computerChoice == "paper")
+            return true;
+        else    
+            return false;
+    }
+}
+
+function playRound(playerChoice, computerChoice)
+{
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if(playerChoice == computerChoice)
+    {
+        bot++;
+        user++;
+    }
+    else if(roundResult(playerChoice, computerChoice) == true)
+    {
+        user++;
+    }
+    else
+    {
+        bot++;
     }
 
-    function roundResult(playerChoice, computerChoice)
-    {
-        if(playerChoice == "rock")
-        {
-            if(computerChoice == "scissors")
-                return true;
-            else
-                return false;
-        }
-        else if(playerChoice == "paper")
-        {
-            if(computerChoice == "rock")
-                return true;
-            else    
-                return false;
-        }
-        else 
-        {
-            if(computerChoice == "paper")
-                return true;
-            else    
-                return false;
-        }
-    }
+    let output = document.querySelector("#output");
 
-    function playRound(playerChoice, computerChoice)
+    if(bot === 5 || user === 5)
     {
-        playerChoice = playerChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
+        output.innerHTML = `Match has been Ended. Final Scores are : 
+                            <div id="botResult">Bot: ${bot}</div>
+                            <div id="userResult">User: ${user}</div>`;
 
-        if(playerChoice == computerChoice)
+        if(bot === 5)
         {
-            bot++;
-            user++;
-        }
-        else if(roundResult(playerChoice, computerChoice) == true)
-        {
-            user++;
+            document.querySelector("div > #userResult").style.backgroundColor = "red"; 
+            document.querySelector("div > #botResult").style.backgroundColor = "lightgreen"; 
         }
         else
         {
-            bot++;
+            document.querySelector("div > #userResult").style.backgroundColor = "lightgreen"; 
+            document.querySelector("div > #botResult").style.backgroundColor = "red"; 
         }
-
-        let output = document.querySelector("#output");
-
-        if(bot === 5 || user === 5)
-        {
-            output.innerHTML = `Match has been Ended. Final Scores are : 
-                                <div id="botResult">Bot: ${bot}</div>
-                                <div id="userResult">User: ${user}</div>`;
-
-            if(bot === 5)
-            {
-                document.querySelector("div > #userResult").style.backgroundColor = "red"; 
-                document.querySelector("div > #botResult").style.backgroundColor = "lightgreen"; 
-            }
-            else
-            {
-                document.querySelector("div > #userResult").style.backgroundColor = "lightgreen"; 
-                document.querySelector("div > #botResult").style.backgroundColor = "red"; 
-            }
-            bot = 0;
-            user = 0;
-            matchRun = false;
-        }
-        else
-        {
-            output.innerText = `Bot: ${bot}
-                                User: ${user}`;
-        }
+        bot = 0;
+        user = 0;
+        matchRun = false;
     }
+    else
+    {
+        output.innerText = `Bot: ${bot}
+                            User: ${user}`;
+    }
+}
 
-    let play = document.querySelector("#start");
-    let user = 0;
-    let bot = 0;
-    let matchRun = false;
+let play = document.querySelector("#start");
+let user = 0;
+let bot = 0;
+let matchRun = false;
 
-    let textBox = document.querySelector("#message");
+let textBox = document.querySelector("#message");
 
-    textBox.addEventListener('keyup', function(event){
-        if(matchRun === false)  
-            return;
+textBox.addEventListener('keyup', function(event){
+    if(matchRun === false)  
+        return;
 
-        let computerChoice = getComputerChoice();
+    let computerChoice = getComputerChoice();
 
-        if(event.key === 'p' || event.key === 'P')
-            playRound("paper", computerChoice);
-        else if(event.key === 'r' || event.key === 'R')    
-            playRound("rock", computerChoice);
-        else if(event.key === 's' || event.key === 'S')
-            playRound("scissors", computerChoice);
-        textBox.value = '';
-    }); 
+    if(event.key === 'p' || event.key === 'P')
+        playRound("paper", computerChoice);
+    else if(event.key === 'r' || event.key === 'R')    
+        playRound("rock", computerChoice);
+    else if(event.key === 's' || event.key === 'S')
+        playRound("scissors", computerChoice);
+    textBox.value = '';
+}); 
 
-    play.addEventListener('click', function() {
-        let resultLog = document.querySelector("#output");
+play.addEventListener('click', function() {
+    let resultLog = document.querySelector("#output");
 
-        resultLog.innerText = "Press R for Rock, S for Scissors or P for Paper!";
-        textBox.value = "";
-        textBox.focus();
-        matchRun = true;
-    });
+    resultLog.innerText = "Press R for Rock, S for Scissors or P for Paper!";
+    textBox.value = "";
+    textBox.focus();
+    matchRun = true;
+});
